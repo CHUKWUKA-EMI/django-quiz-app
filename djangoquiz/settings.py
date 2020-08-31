4f3fa2c207101cc5f3a542560c19a0c182d457aa
Chukwuka Emi <emichukwuka@gmail.com>
2020-08-31 04:41:28 +0100

    configured whitenoise

--- djangoquiz/settings.py
+++ djangoquiz/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'djangoquiz.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DATABASE_NAME"),
-        'USER': os.getenv("DATABASE_USER"),
-        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
-        'HOST': os.getenv("DATABASE_HOST"),
-        'PORT': '5432',
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.getenv("DATABASE_NAME"),
+    #     'USER': os.getenv("DATABASE_USER"),
+    #     'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+    #     'HOST': os.getenv("DATABASE_HOST"),
+    #     'PORT': '5432',
+    # },
     'DATABASE_URL':os.getenv("DATABASE_URL")
 
 }
